d9f30e5b9373596bd9e2953b2155fe57675f9d22
Nadja Zollo <nadja.zollo@gmail.com>
2015-04-27 17:37:43 +0200

    Start with event map.

--- client/templates/includes/login.js
+++ client/templates/includes/login.js
@@ -6,3 +6,40 @@ Template.loginForm.helpers({
     return false;
   }
 });
+
+Template.loginForm.events({
+  // We want to listen for the 'submit' event of the <form> to enable a user
+  // to submit the form via 'Enter' key besides using the 'Submit' button.
+  "submit #login-form": function(event, template) {
+    var username,
+      password;
+
+    event.preventDefault();
+
+    username = template.find("#login-username").value;
+    password = template.find("#login-password").value;
+
+
+  },
+  "click #create-account-link": function(event, template) {
+
+  }
+});
+
+Template.createAccountForm.events({
+  // We want to listen for the 'submit' event of the <form> to enable a user
+  // to submit the form via 'Enter' key besides using the 'Submit' button.
+  "submit #create-account-form": function(event, template) {
+    var username,
+      password,
+      passwordAgain;
+
+    event.preventDefault();
+
+    username = template.find("#account-username").value;
+    password = template.find("#account-password").value;
+    passwordAgain = template.find("#account-password-again").value;
+
+  }
+
+});
