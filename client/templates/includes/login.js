3f2e9ed166edfda7db942dd1a15be5afaef5230f
Nadja Zollo <nadja.zollo@gmail.com>
2015-04-30 18:34:41 +0200

    Implement helper functions to validate form input. Start with 'Create account' and 'Login' logic.

--- client/templates/includes/login.js
+++ client/templates/includes/login.js
@@ -3,7 +3,7 @@ Template.loginForm.helpers({
     return true;
   },
   createAccount: function() {
-    return false;
+    return Session.equals("formContainer", "createAccountForm");
   }
 });
 
@@ -16,12 +16,24 @@ Template.loginForm.events({
 
     event.preventDefault();
 
-    username = template.find("#login-username").value;
+    username = trimInput(template.find("#login-username").value);
     password = template.find("#login-password").value;
 
-
+    if (!isEmpty(username) && !isEmpty(password)) {
+      // The Meteor.loginWithPassword() function is provided by the 'accounts-password' package.
+      Meteor.loginWithPassword(username, password, function(error) {
+        if (error) {
+          // Let the user know that the login failed, e.g. if a user could
+          // not be found or if the user entered an incorrect password.
+          console.log(error.error);
+        }
+      });
+    }
   },
   "click #create-account-link": function(event, template) {
+    event.preventDefault();
+    // Use the global Session object to specify the current form to display. 
+    Session.set("formContainer", "createAccountForm");
 
   }
 });
@@ -36,10 +48,59 @@ Template.createAccountForm.events({
 
     event.preventDefault();
 
-    username = template.find("#account-username").value;
+    username = trimInput(template.find("#account-username").value);
     password = template.find("#account-password").value;
-    passwordAgain = template.find("#account-password-again").value;
 
+    if (!isEmpty(username) &&
+      !isEmpty(password) &&
+      isValidUsername(username) &&
+      isValidPassword(password)) {
+        // The Accounts.createUser() function is provided by the 'accounts-password' package.
+        Accounts.createUser({
+          username: username,
+          password: password
+        }, function(error) {
+          if (error) {
+            // Let the user know that the creation of an account failed.
+            console.log(error.error);
+          }
+        });
+    }
   }
-
 });
+
+// Helper function to check if the user entered a name of at least 3 characters.
+function isValidUsername(input) {
+  if (input.length >= 3) {
+    return true;
+  } else {
+    console.log("Your name should be at least 3 characters long.");
+    return false;
+  }
+}
+
+// Helper function to check if the user entered a password of at least 6 characters.
+function isValidPassword(input) {
+  if (input.length >= 6) {
+    return true;
+  } else {
+    console.log("Your password should be at least 6 characters long.");
+    return false;
+  }
+}
+
+// Helper function to check if the input value is empty.
+function isEmpty(input) {
+  if (!input || input === "") {
+    console.log("Please do not leave any field empty.");
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// Helper function to remove whitespace before and after the input value.
+// cf. http://blog.benmcmahen.com/post/41741539120/building-a-customized-accounts-ui-for-meteor
+function trimInput(input) {
+  return input.replace(/^\s*|\s*$/g, "");
+}
