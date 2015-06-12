f1606f0ef258bf4f182f10d905aa0f13c0d48904
Nadja Zollo <nadja.zollo@gmail.com>
2015-06-12 11:37:59 +0200

    Add publication of 'Identifications' collection. Still TODOs.

--- server/publications.js
+++ server/publications.js
@@ -6,3 +6,19 @@ Meteor.publish("trainings", function() {
   // TODO: Add filter for trainings which are out of date
   return Trainings.find();
 });
+
+// We want to retrieve documents which were created by the currently logged-in user
+// in order to only publish data from the server that belongs to the current user.
+Meteor.publish("ownIdentifications", function() {
+  var currentUserId = this.userId;
+  // TODO Add condition to show only identification documents of the current training
+  return Identifications.find({createdBy: currentUserId});
+});
+
+Meteor.publish("otherIdentifications", function() {
+  //TODO
+});
+
+Meteor.publish("links", function() {
+  return Links.find();
+});
