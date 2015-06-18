7ea27a970dc98e50e783626b1f6e583e2f3f1100
Nadja Zollo <nadja.zollo@gmail.com>
2015-06-18 17:42:42 +0200

    Implement 'selectNodeElement()' function to handle selection/deselection of elements instead of two separate functions. Remove listening to 'focusout' events. Remove some console logs.

--- client/templates/includes/my_ids.js
+++ client/templates/includes/my_ids.js
@@ -3,6 +3,8 @@ Template.myIds.onRendered(function() {
   var margin,
     width,
     height,
+    xPos,
+    yPos,
     radius,
     placeHolderTxt,
     selectedNode,
@@ -43,6 +45,8 @@ Template.myIds.onRendered(function() {
 
   width = 768 - margin.left - margin.right;
   height = 1024 - margin.top - margin.bottom;
+  xPos = width / 2;
+  yPos = height / 3 * 1.5;
   radius = 35;
   placeHolderTxt = "I identify with...";
   isFixed = false;
@@ -60,8 +64,10 @@ Template.myIds.onRendered(function() {
     rootNode = {
       level: 0,
       fixed: true,
-      x: width / 2,
-      y: height / 3 * 1.5,
+      x: xPos,
+      y: yPos,
+      px: xPos,
+      py: yPos,
       children: [],
       createdBy: currentUser._id,
       trainingId: currentTrainingId
@@ -75,14 +81,15 @@ Template.myIds.onRendered(function() {
   }
 
   dragstart = function(d) {
-    console.log("dragstart event ", d3.event);
+    //console.log("dragstart event ", d3.event);
     d3.event.sourceEvent.stopPropagation();
     d3.event.sourceEvent.preventDefault();
     force.stop();
   };
 
   dragmove = function(d) {
-    console.log("dragmove event ", d3.event);
+    // console.log("dragmove event ", d3.event);
+    console.log("DRAGMOVE item with data: ", d);
     d.px += d3.event.dx;
     d.py += d3.event.dy;
     d.x += d3.event.dx;
@@ -91,7 +98,7 @@ Template.myIds.onRendered(function() {
   };
 
   dragend = function(d) {
-    console.log("dragend event ", d3.event);
+    //console.log("dragend event ", d3.event);
     d.fixed = true;
     updateDOM();
     force.resume();
@@ -124,7 +131,6 @@ Template.myIds.onRendered(function() {
 
   // Handle the mousedown event for the outer svgGroup.
   mousedown = function() {
-    console.log("svg mousedown");
     if (!mousedownNode) {
       // TODO(nz): Implement Zoom+Pan behavior
       return;
@@ -138,8 +144,6 @@ Template.myIds.onRendered(function() {
       return;
     }
 
-    deselectElement();
-
     // We update the coordinates of the dragLine during mousemove to draw a line
     // from the mousedownNode to the current mouse position.
     dragLine
@@ -152,7 +156,6 @@ Template.myIds.onRendered(function() {
 
   // Handle the mouseup event for the outer svgGroup.
   mouseup = function() {
-    console.log("svg mouseup");
     var newNodePos,
       node,
       newNodeId,
@@ -164,11 +167,8 @@ Template.myIds.onRendered(function() {
     dragLine.attr("class", "drag-line-hidden");
 
     if (!mousedownNode || mouseupNode === mousedownNode) {
-      console.log("up == down");
-      selectedNode = null;
-      deselectElement();
+      selectNodeElement(null);
       resetMouseVars();
-      //updateLayout();
       return;
     }
 
@@ -179,6 +179,8 @@ Template.myIds.onRendered(function() {
       fixed: isFixed,
       x: newNodePos[0],
       y: newNodePos[1],
+      px: newNodePos[0],
+      py: newNodePos[1],
       parentId: mousedownNode._id,
       children: [],
       name: placeHolderTxt,
@@ -216,7 +218,7 @@ Template.myIds.onRendered(function() {
 
     // Set the new node as selectedNode.
     selectedNode = Identifications.findOne({"_id": newNodeId});
-    Session.set("selectedElement", selectedNode._id);
+    selectNodeElement(selectedNode._id);
 
     console.log("mousedownNode, ", mousedownNode);
     console.log("selectedNode, ", selectedNode);
@@ -244,7 +246,6 @@ Template.myIds.onRendered(function() {
 
 
   updateLayout = function(idsCollection, linksCollection) {
-    console.log("updateLayout");
     var nodeEnterGroup,
         nodeControls,
         dragIcon,
@@ -285,7 +286,8 @@ Template.myIds.onRendered(function() {
     });
 
     nodeElements.classed("node-selected", function(d) {
-      return selectedNode && d._id === selectedNode._id;
+      // return selectedNode && d._id === selectedNode._id;
+      return Session.equals("selectedElement", d._id);
     });
 
     // Remove any deleted elements
@@ -311,7 +313,8 @@ Template.myIds.onRendered(function() {
           return d._id && d.level > 0;
         },
         "node-selected": function(d) {
-          return selectedNode && d._id === selectedNode._id;
+          // return selectedNode && d._id === selectedNode._id;
+          return Session.equals("selectedElement", d._id);
         }
       });
 
@@ -392,67 +395,42 @@ Template.myIds.onRendered(function() {
       .on("mousedown", function(d) {
         d3.event.stopPropagation();
         mousedownNode = d;
-        // if (mousedownNode === selectedNode) {
-        //   console.log("mousedownNode === selectedNode");
-        //   selectedNode = null;
-        // } else {
-        //   selectedNode = mousedownNode;
-        // }
-
-        deselectElement();
-        selectElement(mousedownNode._id);
-
-        // Position the drag line coordinates
-        dragLine
-          .attr("class", "drag-line")
-          .attr("x1", mousedownNode.x)
-          .attr("y1", mousedownNode.y)
-          .attr("x2", mousedownNode.x)
-          .attr("y2", mousedownNode.y);
-
-        //updateLayout();
+        selectNodeElement(mousedownNode._id);
       })
-      .on("mouseup", function(d) {
-        // if (d3.event.defaultPrevented) {
-        //   return;
-        // }
+      .on("mouseup", function(d) { // mouseup on a Böbbel
         d3.event.stopPropagation();
         mouseupNode = d;
-        if (mouseupNode === mousedownNode) {
+        if (mouseupNode._id === mousedownNode._id) {
           resetMouseVars();
           return;
         }
-
-        //updateLayout();
       })
       .on("keydown", function(d) {
         d3.event.stopPropagation();
+
         if (d3.event.keyCode === 13) {
           d3.event.preventDefault();
           d3.select(this).select("p.txt-input").node().blur();
         }
-      })
-      .on("focusout", function(d) {
+
         if (d.level > 0) {
           var newName,
             inputTxt = d3.select(this).select("p.txt-input");
 
           newName = inputTxt.text();
+
           if (newName === placeHolderTxt || newName === "") {
             Session.set("emptyNode", d._id);
           }
+
           Identifications.update(d._id, {
             $set: {name: newName}
           });
 
-          // deselectElement();
-
-          // if (d === selectedNode) {
-          //   selectedNode = null;
-          // }
-          //updateLayout();
+          if (Session.equals("selectedElement", d._id)) {
+            selectNodeElement(null);
+          }
         }
-        return;
       })
       .on("dblclick", function(d) {
         if (d.level > 0) {
@@ -598,52 +576,67 @@ Template.myIds.onDestroyed(function() {
   // TODO stop autorun
 });
 
-Template.myIds.helpers({
-  warningDialog: function() {
-    return Session.get("emptyNode");
-  }
-});
+// Template.myIds.helpers({
+//   warningDialog: function() {
+//     return Session.get("emptyNode");
+//   }
+// });
+//
+// Template.warning.events({
+//   "click #remove-btn": function(event, template) {
+//     event.preventDefault();
+//     deleteNodeAndLink("emptyNode");
+//   },
+//   "click #enter-btn": function(event, template) {
+//     event.preventDefault();
+//     var targetId = Session.get("emptyNode");
+//     if (targetId) {
+//       d3.select("#gid" + targetId).select("p.txt-input").node().focus();
+//       document.execCommand("selectAll", false, null);
+//       Session.set("emptyNode", null);
+//     }
+//   }
+// });
 
-Template.warning.events({
-  "click #remove-btn": function(event, template) {
-    event.preventDefault();
-    deleteNodeAndLink("emptyNode");
-  },
-  "click #enter-btn": function(event, template) {
-    event.preventDefault();
-    var targetId = Session.get("emptyNode");
-    if (targetId) {
-      d3.select("#gid" + targetId).select("p.txt-input").node().focus();
-      document.execCommand("selectAll", false, null);
-      Session.set("emptyNode", null);
-    }
-  }
-});
 
 
-function deselectElement() {
-  console.log("deselect");
-  console.log("event ", d3.event.type);
-  console.log("eventTarget ", d3.event.target);
-  console.log("eventCurrenTarget ", d3.event.currentTarget);
+/**
+ * Selects (or deselects) a node element (i.e. an identification circle).
+ * If the node is a newly created identification circle it gets selected immediately.
+ * All the other nodes are selectable by user interaction.
+ * We use a Session variable to detect selected or deselected state, respectively.
+ * Depending on that we apply a CSS class, which in turn toggles the control handles for
+ * dragging or deleting a circle.
+ * @param {string} elementId The (database document) id of the node.
+ */
+function selectNodeElement(elementId) {
   var selectedElement = Session.get("selectedElement");
+  if (elementId === selectedElement) {
+    return;
+  }
   if (selectedElement) {
     d3.select("#gid" + selectedElement).classed("node-selected", false);
   }
-  Session.set("selectedElement", null);
-}
-
-function selectElement(elementId) {
-  console.log("select");
-  console.log("event ", d3.event.type);
-  console.log("eventTarget ", d3.event.target);
-  console.log("eventCurrenTarget ", d3.event.currentTarget);
-  d3.select("#gid" + elementId).classed("node-selected", true);
+  if (elementId) {
+    d3.select("#gid" + elementId).classed("node-selected", true);
+  }
   Session.set("selectedElement", elementId);
 }
 
+/**
+ * Deletes a node document (i.e. an identification document) and its associated
+ * link documents from the respective collection.
+ * Since we call this function from inside different event handlers we use a
+ * session key to set the variable in the session.
+ * @param {string} sessionKey The key of the session variable to set.
+ */
 function deleteNodeAndLink(sessionKey) {
-  var nodeId = Session.get(sessionKey);
+  var nodeId,
+    parentId;
+
+  nodeId = Session.get(sessionKey);
+  parentId = Identifications.findOne(nodeId).parentId;
+
   if (nodeId) {
     Links.remove(Links.findOne({"target._id": nodeId})._id, function(error, result){
       if (error) {
@@ -651,6 +644,13 @@ function deleteNodeAndLink(sessionKey) {
       }
     });
     Identifications.remove(nodeId, function(error, result){
+      if (error) {
+        return throwError("Error: " + error.reason);
+      }
+    });
+    Identifications.update(parentId, {
+      $pull: {children: nodeId}
+    }, function(error, result) {
       if (error) {
         return throwError("Error: " + error.reason);
       }
