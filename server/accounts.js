128b59dc2366d9c2bfb4fbdbcad0f5f9fcdf326a
Nadja Zollo <nadja.zollo@gmail.com>
2015-06-25 18:00:00 +0200

    Update the 'attendees' field of the current training document when a new user is created.

--- server/accounts.js
+++ server/accounts.js
@@ -5,5 +5,17 @@ Accounts.onCreateUser(function(options, user) {
     user.profile = options.profile;
   }
 
+  // Add the new user to the 'attendees' field (an array) of the current training document.
+  if (options.profile.currentTraining) {
+    Trainings.update(options.profile.currentTraining, {
+      $push: {attendees: user._id}
+    }, function(error, result) {
+      if (error) {
+        // TODO Improve error handling.
+        console.log("Error while updating document. Reason: ", error.reason);
+      }
+    });
+  }
+
   return user;
 });
