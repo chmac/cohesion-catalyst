ff9a41d538bc64c21f4d334a3f6123bc0f498c57
Hartmut Schirmacher <hartmut@hartmut-schirmacher.de>
2015-08-12 11:06:23 +0200

    added basic interface functions for adding, deleting, and updating IDs for rendering the ID pool

--- client/templates/includes/pool.js
+++ client/templates/includes/pool.js
@@ -5,6 +5,101 @@
  */
 
 
+/**
+  * init code for the "ID Pool" screen
+  */
+Template.idPool.onCreated(function() {
+
+  // array to store the pool of IDs for rendering  
+  Template.idPool.ids = [];
+
+  // Testing
+  var id = Template.idPool.addID("Hello World!","4","purple");
+
+});
+
+/**
+  *   add an ID to the pool of IDs to be rendered
+  */
+Template.idPool.addID = function(text,count,color) {
+
+  var id = {};
+  id.text = text || "wat?";
+  id.count = count || 1;
+  id.color = color || "purple";
+  Template.idPool.ids.push(id);
+
+  // Testing
+  var debug = Template.idPool.getID(id.text);
+  console.log(debug);
+
+};
+
+/**
+  *   search ID by text and return full ID information, or undefined
+  */
+Template.idPool.getID = function(text) {
+  for(var i=0; i<Template.idPool.ids.length; i++) {
+    if(Template.idPool.ids[i].text == text) {
+      return Template.idPool.ids[i];
+    }
+  }
+  return undefined;
+};
+
+/**
+  *   delete ID, identified by text
+  */
+Template.idPool.deleteID = function(text) {
+  for(var i=0; i<Template.idPool.ids.length; i++) {
+    if(Template.idPool.ids[i].text == text) {
+      Template.idPool.ids.slice(i,1);
+      return;
+    }
+  }
+};
+
+/**
+  *   update information for an ID, identified by text
+  */
+Template.idPool.updateID = function(text, newText, newCount, newColor) {
+  for(var i=0; i<Template.idPool.ids.length; i++) {
+    var id = Template.idPool.ids[i];
+    if(id.text == text) {
+      id = {"text": newText || id.text, 
+            "count": newCount || id.count,
+            "color": newColor || id.color};
+      return;
+    }
+  }
+};
+
+
+/** 
+  * draw ID bubble at specified position with specified scale factor
+  * drawingSurface: the SVG area's topmost group element
+  * id: one entry from the ids array
+  * pos: array containing X and Y coordinate of bubble to be drawn
+  * scale: scale factor for bubble's size
+  **/
+Template.idPool.drawBubble = function(drawingSurface, id, pos, scale) {
+
+  var bubbleGroup = drawingSurface.append("g")
+    .attr("transform", "translate(" + pos[0] + "," + pos[1] + ")");
+
+  bubbleGroup.append("circle")
+    .attr("r", 40*scale)
+    .style("fill", id.color);
+
+  bubbleGroup.append("text")
+    .attr("dy", ".3em")
+    .style("text-anchor", "middle")
+    .style("fill", "white")
+    .text(id.text);
+
+};
+
+
  /**
   * Adds a callback to be called when an instance of this template is rendered and inserted
   * into the DOM.
@@ -51,22 +146,8 @@
     .attr("height", height)
     .attr("class", "drawing-surface");
 
-
-  // Dummy group containing circle and text for testing
-  var bubbleGroup = drawingSurface.append("g")
-    .attr("transform", "translate(" + width/2 + "," + height/3 + ")");
-
-  bubbleGroup.append("circle")
-    .attr("r", 40)
-    .style("fill", "purple");
-
-  bubbleGroup.append("text")
-    .attr("dy", ".3em")
-    .style("text-anchor", "middle")
-    .style("fill", "white")
-    .text("Wat?!");
-
-
+  // Testing
+  Template.idPool.drawBubble(drawingSurface, Template.idPool.ids[0], [width/2, height/3], 1.2);
 
 
 }); // end Template.idPool.onRendered()
