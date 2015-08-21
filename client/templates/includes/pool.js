4f5abfe236483e58c4b7d6a7763ad2cdeccea1f7
Hartmut Schirmacher <hartmut@hartmut-schirmacher.de>
2015-08-21 09:56:26 +0200

    added touchMouseEvents module and integrated it into pool

--- client/templates/includes/pool.js
+++ client/templates/includes/pool.js
@@ -15,6 +15,12 @@ var pool = function() {
   /** layout to be used for positioning the bubbles in the pool */
   var layout;
 
+  /** drawing surface to render into */
+  var drawingSurface;
+
+  /** (HACK) remember width and height of drawingSurface */
+  var width,height;
+
   /**
     * init code for the "ID Pool" screen
     */
@@ -33,23 +39,6 @@ var pool = function() {
 
     // initial dummy layout, until all bubbles in pool are known
     layout = new LayoutSameNumPerRow(function(){return 0;});
-
-    subscription = templateInstance.subscribe("otherIdentifications", currentTrainingId,
-      function() {
-        // initial query to populate pool with current set of IDs 
-        Identifications.find({
-          createdBy: {$ne: currentUser._id},
-          trainingId: currentTrainingId,
-          editCompleted: true
-        }).forEach(function(d) { addID(d); });
-        layout = new LayoutSameNumPerRow( function(){return ids.length;}, 
-                                          {"baseBubbleRadius": 40});
-        draw();
-    });
-
-    // set up autorunner to observe IDs that come, go, or change
-  //  templateInstance.autorun(function() {
-  //  }); // autorun()
 
   }); // onCreated()
 
@@ -76,6 +65,35 @@ var pool = function() {
     // We access the template instance at 'this' and store it in a variable.
     templateInstance = this;
 
+    // create the drawingSurface to render into
+    drawingSurface = makeDrawingSurface();
+
+    // set up mouse events
+    touchMouseEvents(drawingSurface, // target
+                     drawingSurface.node(), // container for position calculation
+                     { "test": false,
+                       "dragMove": function(x,y,dx,dy) { d3.event.preventDefault(); layout.scroll(dy); draw(); }
+                     });
+
+    // subscribe to other people's IDs, draw when ready
+    subscription = templateInstance.subscribe("otherIdentifications", currentTrainingId,
+      function() {
+        // initial query to populate pool with current set of IDs 
+        Identifications.find({
+          createdBy: {$ne: currentUser._id},
+          trainingId: currentTrainingId,
+          editCompleted: true
+        }).forEach(function(d) { addID(d); });
+        layout = new LayoutSameNumPerRow( function(){return ids.length;}, 
+                                          {"baseBubbleRadius": 40});
+        // set width and height in layout
+        layout.setDimensions(width,height);
+
+        // draw bubble pool
+        draw();
+    });
+
+
     // set up autorunner to observe IDs that come, go, or change
     templateInstance.autorun(function() {
 
@@ -101,8 +119,6 @@ var pool = function() {
 
     }); // end autorun()
 
-    // initially render the ID pool 
-    draw();
 
   });  // onRendered()
 
@@ -192,6 +208,85 @@ var pool = function() {
 
   }; // XXX
 
+  /**
+    * add the SVG container to draw into 
+    *
+    */ 
+  var makeDrawingSurface = function() {
+   var margin,
+      //width,
+      //height,
+      svgViewport,
+      drawingSurface;
+
+    // We create a margin object following the D3 margin convention.
+    // cf. http://bl.ocks.org/mbostock/3019563
+    margin = {
+      top: 20,
+      right: 10,
+      bottom: 20,
+      left: 10
+    };
+
+    // We define the inner dimensions of the drawing area.
+    width = 768 - margin.left - margin.right;
+    height = 1024 - margin.top - margin.bottom;
+
+    // remove all existing children of the drawing area
+    //d3.select("#ids-graph").selectAll("*").remove();
+    d3.select("#ids-graph").select("svg").remove();
+    // We create our outermost <svg> and append it to the existing <div id='ids-graph'>.
+    svgViewport = d3.select("#ids-graph").append("svg")
+      .attr("id", "ids-vis")
+      .attr("viewBox", "0 0 " + (width + margin.left + margin.right) + " " + (height + margin
+        .top + margin.bottom))
+      .attr("preserveAspectRatio", "xMidYMid meet");
+
+    // We append a <g> element that translates the origin to the top-left
+    // corner of the drawing area.
+    drawingSurface = svgViewport.append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    // We append a <rect> as the 'canvas' to draw on and as target of 'pointer events'.
+    drawingSurface.append("rect")
+      .attr("width", width)
+      .attr("height", height)
+      .attr("class", "drawing-surface");
+
+    return drawingSurface;
+
+  };
+
+  /**
+    * draw the current set of bubbles with some magic layouting
+    */
+  draw = function() {
+
+    // do we know where to draw to?
+    if(drawingSurface == undefined) {
+      console.log("no drawing surface yet");
+      return;
+    }
+
+    // remove all scene objects from last rendered frame
+    drawingSurface.selectAll(".scene_obj").remove();
+
+    // for each bubbles, let the layout determine if/where to draw it, and do so  
+    for(var i=0; i<ids.length; i++) {
+      // is there something in this slot in the IDs array? 
+      if(ids[i] != undefined) {
+        // let layout compute position and scale factor
+        var res = layout.getPositionAndSize(i);
+        // draw it at all?
+        if(res != undefined) {
+          // draw single bubble
+          drawBubble(drawingSurface, ids[i], res.x, res.y, 40, res.scale);
+        }
+      }
+    }
+
+  }; // draw()
+
   /** 
     * draw a single ID bubble centered around specified position 
     * with specified radius and scale factor
@@ -205,7 +300,8 @@ var pool = function() {
 
     //console.log("draw bubble " + id.text + " at " + x + " / " + y + " scale " + scale);
     var bubbleGroup = drawingSurface.append("g")
-      .attr("transform", "translate(" + (x) + "," + (y) + ")");
+      .attr("transform", "translate(" + (x) + "," + (y) + ")")
+      .attr("class", "scene_obj"); // mark as a scene object, so can be deleted for next frame
 
     bubbleGroup.append("circle")
       .attr("r", radius)
@@ -239,85 +335,6 @@ var pool = function() {
         .text(id.text);
 
   }; // drawBubble()
-
-  /**
-    * draw the current set of bubbles with some magic layouting
-    */
-  draw = function() {
-
-    var margin,
-      width,
-      height,
-      svgViewport,
-      drawingSurface;
-
-    // We create a margin object following the D3 margin convention.
-    // cf. http://bl.ocks.org/mbostock/3019563
-    margin = {
-      top: 20,
-      right: 10,
-      bottom: 20,
-      left: 10
-    };
-
-    // We define the inner dimensions of the drawing area.
-    width = 768 - margin.left - margin.right;
-    height = 1024 - margin.top - margin.bottom;
-
-    // remove all existing children of the drawing area
-    //d3.select("#ids-graph").selectAll("*").remove();
-    d3.select("#ids-graph").select("svg").remove();
-    // We create our outermost <svg> and append it to the existing <div id='ids-graph'>.
-    svgViewport = d3.select("#ids-graph").append("svg")
-      .attr("id", "ids-vis")
-      .attr("viewBox", "0 0 " + (width + margin.left + margin.right) + " " + (height + margin
-        .top +
-        margin.bottom))
-      .attr("preserveAspectRatio", "xMidYMid meet");
-
-    // We append a <g> element that translates the origin to the top-left
-    // corner of the drawing area.
-    drawingSurface = svgViewport.append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    // We append a <rect> as the 'canvas' to draw on and as target of 'pointer events'.
-    drawingSurface.append("rect")
-      .attr("width", width)
-      .attr("height", height)
-      .attr("class", "drawing-surface");
-
-    // set width and height in layout
-    layout.setDimensions(width,height);
-
-      // for each bubbles, let the layout determine if/where to draw it, and do so  
-    for(var i=0; i<ids.length; i++) {
-      // is there something in this slot in the IDs array? 
-      if(ids[i] != undefined) {
-        // let layout compute position and scale factor
-        var res = layout.getPositionAndSize(i);
-        // draw it at all?
-        if(res != undefined) {
-          // draw single bubble
-          drawBubble(drawingSurface, ids[i], res.x, res.y, 40, res.scale);
-        }
-      }
-    }
-
-    // debugging: scroll on click/shift-click
-    d3.select("#ids-vis").on("mousedown", function(d) {
-      // console.log("move!");
-        if(d3.event.shiftKey) {
-          layout.scroll(-10);
-        } else {
-          layout.scroll(10);
-        }
-        draw();
-    }); // d3.select()
-
-    return "yes, I have drawn the pool!";
-
-  }; // draw()
-
 
   // Test adding and removing ids
   Template.idPool.events({
