f969290a2682016b972fc0a9ab29fde44aa09807
Nadja Zollo <nadja.zollo@gmail.com>
2015-09-21 16:20:38 +0200

    Dummy functionality to test publish/subscribe of 'meta collection'.

--- client/templates/includes/network.js
+++ client/templates/includes/network.js
@@ -1,6 +1,35 @@
 
 Template.idNetwork.onCreated(function() {
-  console.log("created template idNetwork");
-  Meteor.call("testCall", "p1");
-  console.log("continuing in client template after testCall...");
+  var currentUser,
+    currentTrainingId;
+
+  currentUser = Meteor.user();
+  currentTrainingId = currentUser.profile.currentTraining;
+  // console.log("created template idNetwork");
+  // Meteor.call("testCall", "p1");
+  // console.log("continuing in client template after testCall...");
+  this.subscribe("networkIdentifications", currentTrainingId);
+});
+
+Template.idNetwork.helpers({
+  ids: function() {
+    // MetaCollection.find( {createdBy : {$exists:true}, $where:"this.createdBy.length>1"} )
+    return MetaCollection.find({
+      $nor: [
+        {
+          createdBy: {
+            $exists: false
+          }
+        }, {
+          createdBy: {
+            $size: 0
+          }
+        }, {
+          createdBy: {
+            $size: 1
+          }
+        }
+      ]
+    });
+  }
 });
