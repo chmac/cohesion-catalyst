75bb6d8c3dc605ba65d2c64ec692a16a552273f2
Nadja Zollo <nadja.zollo@gmail.com>
2016-03-15 19:06:04 +0100

    Add collections and Schemas to be shared between admin and app.

---
+++ common/lib/collections/users.js
@@ -0,0 +1,78 @@
+// We define and attach a schema to the Meteor.users collection in order to
+// make the collection editable via the admin dashboard.
+// The following schema is borrowed from the 'Collection2' example provided at
+// [as of 2015-12-14] https://github.com/aldeed/meteor-collection2
+Schemas.UserProfile = new SimpleSchema({
+  name: {
+    type: String
+  },
+  avatar: {
+    type: String,
+    optional: true
+  },
+  currentTraining: {
+    type: String,
+    optional: true
+  }
+});
+
+Schemas.User = new SimpleSchema({
+  username: {
+    type: String,
+    // For accounts-password, either emails or username is required, but not both. It is OK to make this
+    // optional here because the accounts-password package does its own validation.
+    // Third-party login packages may not require either.
+    optional: true
+  },
+  emails: {
+    type: Array,
+    // For accounts-password, either emails or username is required, but not both. It is OK to make this
+    // optional here because the accounts-password package does its own validation.
+    // Third-party login packages may not require either.
+    optional: true
+  },
+  "emails.$": {
+    type: Object,
+    optional:true
+  },
+  "emails.$.address": {
+    type: String,
+    regEx: SimpleSchema.RegEx.Email,
+    optional:true
+  },
+  "emails.$.verified": {
+    type: Boolean,
+    optional:true
+  },
+  createdAt: {
+    type: Date
+  },
+  profile: {
+    type: Schemas.UserProfile,
+    optional: true
+  },
+  // We add the services field to our schema (needed when using any of the accounts packages).
+  services: {
+    type: Object,
+    optional: true,
+    blackbox: true
+  },
+  // We add `roles` to our schema because we use the 'meteor-roles' package.
+  // We specify [String] as type since we do not use role groups.
+  // If this changes in the future, we have to specify the type as 'Object'.
+  roles: {
+    type: [String],
+    optional: true
+  },
+  blocked : {
+    type: Boolean,
+    optional: true
+  },
+  // In order to avoid an 'Exception in setInterval callback' from Meteor
+  heartbeat: {
+      type: Date,
+      optional: true
+  }
+});
+
+Meteor.users.attachSchema(Schemas.User);
---
+++ common/lib/startup.js
@@ -0,0 +1,52 @@
+// On server startup, if some of the collections int database are empty,
+// we create some initial data.
+Meteor.startup(function() {
+
+  if (Meteor.users.find().count() === 0) {
+    // Define the users who have admin rights and adding those users to roles
+    // Borrowed from the example at https://github.com/alanning/meteor-roles
+    var users = [
+      {name:"nadeschda",email:"nadja.zollo@gmail.com",roles:["admin"]},
+      {name:"haschi",email:"hartmut@hartmut-schirmacher.de",roles:["admin"]},
+      {name:"steff",email:"mail@stefanie-rathje.com",roles:["admin"]}
+    ];
+
+    _.each(users, function (user) {
+      var id;
+
+      id = Accounts.createUser({
+        username: user.name,
+        // email: user.email,
+        password: "cocacoma",
+        profile: { name: user.name }
+      });
+
+      if (user.roles.length > 0) {
+        // Need _id of existing user record so this call must come
+        // after `Accounts.createUser` or `Accounts.onCreate`
+        Roles.addUsersToRoles(id, user.roles);
+      }
+
+      // Add the email address for each admin user and mark it as verified.
+      Accounts.addEmail(id, user.email, true);
+
+    });
+  }
+
+
+  // Fixture training data
+  if (Trainings.find().count() === 0) {
+    var someday;
+
+    // Dummy date in the future
+    someday = new Date(2016, 8, 30);
+    Trainings.insert({
+      title: "Master Training",
+      description: "Team building master class.",
+      date: someday,
+      players: [],
+      isCurrentTraining: true
+    });
+  } // end Trainings
+
+}); // end startup()
