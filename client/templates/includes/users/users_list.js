f5706109f01e912d139ca496b001bc2e1caa410a
Nadja Zollo <nadja.zollo@gmail.com>
2016-03-17 10:33:23 +0100

    Add tabular data tables templates and helpers.

--- client/templates/includes/users/users_list.js
+++ client/templates/includes/users/users_list.js
@@ -3,3 +3,27 @@ Template.usersList.helpers({
     return Meteor.users.find();
   }
 });
+
+
+
+
+
+Template.userBlockCell.onRendered(function() {
+  var templateInstance = this;
+  $(".ui.checkbox").checkbox();
+});
+
+
+Template.userBlockCell.events({
+  "change .ui.checkbox": function(event, template) {
+    if (template.$(event.currentTarget).hasClass("checked")) {
+      console.log("********* Block user");
+      console.log("this._id ", this._id);
+      console.log("this.profile.name ", this.profile.name);
+    } else {
+      console.log("********* Unblock user");
+      console.log("this._id ", this._id);
+      console.log("this.profile.name ", this.profile.name);
+    }
+  }
+});
