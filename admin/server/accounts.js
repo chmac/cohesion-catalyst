617d675eed2ebba2e4854c2a0512b1d6818bf292
Nadja Zollo <nzollo@beuth-hochschule.de>
2016-03-21 14:40:06 +0100

    Add JSDoc.

--- admin/server/accounts.js
+++ admin/server/accounts.js
@@ -1,4 +1,14 @@
-// http://docs.meteor.com/#/full/accounts_validateloginattempt
+/**
+ * Validates the attempted login.
+ * This method is provided by Meteor's account system.
+ * cf. http://docs.meteor.com/#/full/accounts_validateloginattempt
+ *
+ * Here, we check if the user who tries to log in has admin rights.
+ * @param {Object} attemptInfo - An object containing information about
+ * the login attempt (e.g. attemptInfo.user holds the Meteor user object).
+ * @return {Boolean} True if attempted login is successful, otherwise a
+ * Meteor.Error is thrown for non-admin users which aborts the login.
+ */
 Accounts.validateLoginAttempt(function(attemptInfo) {
   var userId = attemptInfo.user._id;
   if (!Roles.userIsInRole(userId, "admin")) {
