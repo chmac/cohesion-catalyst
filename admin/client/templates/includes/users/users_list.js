8e956acaa0376f3c05c7ed3c37777827779f9f40
Nadja Zollo <nadja.zollo@gmail.com>
2016-03-29 17:29:35 +0200

    Start delete user action.

--- admin/client/templates/includes/users/users_list.js
+++ admin/client/templates/includes/users/users_list.js
@@ -29,3 +29,51 @@ Template.userBlockCell.events({
     }
   }
 });
+
+
+
+// ------------------------------------------------------------------------ //
+// userDeleteCell
+// A subtemplate to be included within 'usersList' template.
+// ------------------------------------------------------------------------ //
+
+Template.userDeleteCell.events({
+  "click .user-delete-button": function(event, template) {
+    event.preventDefault();
+    Session.set("userToDeleteName", this.profile.name);
+    Session.set("userToDeleteID", this._id);
+    console.log("this._id ", this._id);
+    console.log("this.profile.name ", this.profile.name);
+    $(".ui.basic.modal").modal("show");
+  }
+});
+
+
+
+// ------------------------------------------------------------------------ //
+// userDeleteApproveModal
+// A subtemplate to be included within 'usersList' template.
+// ------------------------------------------------------------------------ //
+
+Template.userDeleteApproveModal.onRendered(function() {
+  $(".ui.basic.modal").modal({
+    closable: false,
+    onDeny: function() {
+      console.log("Cancelled!");
+    },
+    onHidden: function() {
+      Session.set("userToDeleteName", null);
+      Session.set("userToDeleteID", null);
+    },
+    onApprove: function() {
+      // TODO call method to delete document
+      console.log("Approved!");
+    }
+  });
+});
+
+Template.userDeleteApproveModal.helpers({
+  username: function() {
+    return Session.get("userToDeleteName");
+  }
+});
