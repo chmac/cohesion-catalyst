b4f73e346133b467ca3778fa1884cc17ff111c7f
Nadja Zollo <nadja.zollo@gmail.com>
2016-04-07 17:34:06 +0200

    Publish 'Meteor.roles' collection.

--- admin/server/publications.js
+++ admin/server/publications.js
@@ -10,3 +10,11 @@ Meteor.publish("singleUser", function(id) {
 Meteor.publish("listOfTrainings", function() {
   return Trainings.find();
 });
+
+// The 'Meteor.roles' collection is provided by the 'alanning:roles' package,
+// but it is not automatically published to the client.
+// Here, we make it available to every client and no subscription is required.
+// cf. https://atmospherejs.com/alanning/roles#changes-to-default-meteor-behavior
+Meteor.publish(null, function (){
+  return Meteor.roles.find({});
+});
