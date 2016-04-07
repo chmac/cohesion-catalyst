0d8e71b5d3c36b2dee38260a4f0d7f10a3a3df34
Nadja Zollo <nzollo@beuth-hochschule.de>
2016-04-07 19:18:46 +0200

    Add schemas to be used with 'aldeed:autoform' package.

---
+++ admin/client/templates/includes/users/user_edit.js
@@ -0,0 +1,45 @@
+Template.userEdit.helpers({
+  userEditIsInRole: function(userId, roleName) {
+    return Roles.userIsInRole(userId, roleName);
+  }
+  // user: function() {
+  //   var userDoc = Meteor.users.findOne(Router.current().params._id);
+  //   if (userDoc) {
+  //     return userDoc;
+  //   }
+  //   return false;
+  // }
+});
+
+Template.userEdit.events({
+  "click .user-edit-add-role": function(event, template) {
+    var roleName = event.target.dataset.roleName;
+    var targetUserId = event.target.dataset.userId;
+    Meteor.call("user.edit.add.role", targetUserId, roleName, function(error, result) {
+      if (error) {
+        if (error.error === "user.edit.add.role.not-authorized") {
+          sAlert.error("You need to have admin rights to add a role to a user.");
+        } else {
+          sAlert.error("An unexpected error occured: ", error.reason);
+        }
+      } else {
+        sAlert.success("User role successfully added.");
+      }
+    });
+  },
+  "click .user-edit-remove-role": function(event, template) {
+    var roleName = event.target.dataset.roleName;
+    var targetUserId = event.target.dataset.userId;
+    Meteor.call("user.edit.remove.role", targetUserId, roleName, function(error, result) {
+      if (error) {
+        if (error.error === "user.edit.remove.role.not-authorized") {
+          sAlert.error("You need to have admin rights to remove a role from a user.");
+        } else {
+          sAlert.error("An unexpected error occured: ", error.reason);
+        }
+      } else {
+        sAlert.success("User role successfully removed");
+      }
+    });
+  }
+});
