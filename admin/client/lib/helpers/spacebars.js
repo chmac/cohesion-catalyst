a0156a2617b94a0a117b78319c637c6d171b3e6a
Nadja Zollo <nzollo@beuth-hochschule.de>
2016-04-10 14:19:27 +0200

    Register global template helpers for avatar selection and training.

--- admin/client/lib/helpers/spacebars.js
+++ admin/client/lib/helpers/spacebars.js
@@ -5,3 +5,26 @@
 Template.registerHelper("isAdmin", function(userId) {
   return Roles.userIsInRole(userId, "admin");
 });
+
+Template.registerHelper("avatarOptions", function() {
+  return [
+    {label: "Wink", value: "#smiley-wink"},
+    {label: "Chuckle", value: "#smiley-chuckle"},
+    {label: "Smirk", value: "#smiley-smirk"},
+    {label: "LOL", value: "#smiley-lol"},
+    {label: "Smile", value: "#smiley-smile"},
+    {label: "Nerd", value: "#smiley-nerd"},
+    {label: "Star", value: "#smiley-star"},
+    {label: "Heart", value: "#smiley-heart"},
+    {label: "Cool", value: "#smiley-cool"}
+  ];
+});
+
+Template.registerHelper("trainingOptions", function() {
+  return Trainings.find().map(function(training) {
+    return {
+      label: training.title + " - " + moment(training.date).format("MMMM Do YYYY") + " (ID: " + training._id + ")",
+      value: training._id
+    };
+  });
+});
