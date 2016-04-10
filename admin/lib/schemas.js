fd94564d7321fa0a7b270fb28439f5e835f99a57
Nadja Zollo <nadja.zollo@gmail.com>
2016-04-10 14:13:51 +0200

    Add different user collection schema to be used with autoForm.

--- admin/lib/schemas.js
+++ admin/lib/schemas.js
@@ -11,3 +11,57 @@ AdminSchemas.ChangePassword = new SimpleSchema({
     min: 6
   }
 });
+
+AdminSchemas.UserProfile = new SimpleSchema({
+  name: {
+    type: String
+  },
+  avatar: {
+    type: String,
+    optional: true
+  },
+  currentTraining: {
+    type: String,
+    optional: true
+  }
+});
+
+AdminSchemas.UserData = new SimpleSchema({
+  username: {
+    type: String,
+    // For accounts-password, either emails or username is required, but not both. It is OK to make this
+    // optional here because the accounts-password package does its own validation.
+    // Third-party login packages may not require either.
+    optional: true
+  },
+  profile: {
+    type: AdminSchemas.UserProfile,
+    optional: true
+  },
+  emails: {
+    type: Array,
+    // For accounts-password, either emails or username is required, but not both. It is OK to make this
+    // optional here because the accounts-password package does its own validation.
+    // Third-party login packages may not require either.
+    optional: true
+  },
+  "emails.$": {
+    type: Object,
+    optional:true
+  },
+  "emails.$.address": {
+    type: String,
+    regEx: SimpleSchema.RegEx.Email,
+    optional:true
+  },
+  "emails.$.verified": {
+    type: Boolean,
+    optional:true
+  },
+  role: {
+    type: String,
+    optional: true,
+    allowedValues: ["admin"],
+    label: "Role"
+  }
+});
