d072882e45cf72ab5e263c97a50f5f72102ecfb2
Nadja Zollo <nzollo@beuth-hochschule.de>
2016-04-20 11:26:50 +0200

    Remove obsolete code passages and files related to collections and schemas since this is now handled via coca:common package.

--- app/lib/collections/meta_collection.js
+++ app/lib/collections/meta_collection.js
@@ -1,30 +1,11 @@
-(function() {
-// MetaCollection = new Mongo.Collection("metaCollection");
-//
-// // ------------------------------------------------------------------------------------------- //
-// // -------------------------------------- Schema ---------------------------------------- //
-// // ------------------------------------------------------------------------------------------- //
-//
-// Schemas.MetaCollection = new SimpleSchema({
-//   name: {
-//     type: String
-//   },
-//   standardizedName: {
-//     type: String
-//   },
-//   color: {
-//     type: String
-//   },
-//   createdBy: {
-//     type: [String]
-//   },
-//   createdAtTraining: {
-//     type: String
-//   }
-// });
-//
-// MetaCollection.attachSchema(Schemas.MetaCollection);
+/**
+ * The 'MetaCollection' collection and its schema are defined
+ * within the private package coca:common which is shared between
+ * this project and the admin app project.
+ */
 
+
+(function() {
 // ------------------------------------------------------------------------------------------- //
 // ----------------------------------------- Methods ----------------------------------------- //
 // ------------------------------------------------------------------------------------------- //
