711490a68c6e0e78b2341612d4de0495c856b1bb
Nadja Zollo <nzollo@beuth-hochschule.de>
2016-05-03 16:51:08 +0200

    Publish user roles.

--- app/server/publications.js
+++ app/server/publications.js
@@ -1,4 +1,23 @@
 (function() {
+
+// The 'Meteor.roles' collection is provided by the 'alanning:roles' package,
+// but it is not automatically published to the client.
+// Here, we make it available to every client and no subscription is required.
+// cf. https://atmospherejs.com/alanning/roles#changes-to-default-meteor-behavior
+Meteor.publish(null, function (){
+  return Meteor.roles.find({});
+});
+
+Meteor.publish("userWithRoles", function() {
+    return Meteor.users.find({
+      _id: this.userId
+    }, {
+      fields: {
+        roles: 1
+      }
+    });
+});
+
 Meteor.publish("avatars", function() {
   return Avatars.find();
 });
