68aff20ce26e7f3fdec59a47fab3751b6f1d9182
Nadja Zollo <nzollo@beuth-hochschule.de>
2016-05-25 20:27:09 +0200

    Test fullscreen. Give players fixed position (not final). Make players reactive.

--- app/client/templates/application/bullseye/bullseye_players.js
+++ app/client/templates/application/bullseye/bullseye_players.js
@@ -1,9 +1,10 @@
 (function() {
 
   var drawingSurface;
+  var playerList = [];
 
   Template.bullseyePlayers.onRendered(function() {
-
+    var templateInstance = this;
     Session.set("canvasSize", document.documentElement.clientHeight);
 
     var avatarSize = 105;
@@ -24,26 +25,136 @@
       .attr("id", "players-group")
       .attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
 
-    var bullseyePlayers = Meteor.users.find({
+    // var bullseyePlayers = Meteor.users.find({
+    //   "profile.currentTraining": Session.get("bullseyeCurrentTraining")
+    // }, {
+    //   fields: {
+    //     profile: 1,
+    //     status: 1
+    //   }
+    // }).fetch();
+    // console.log(bullseyePlayers);
+
+    var playersConfig = configPlayers(avatarSize, margin);
+
+    var bullseyePlayersCursor = Meteor.users.find({
       "profile.currentTraining": Session.get("bullseyeCurrentTraining")
     }, {
       fields: {
-        profile: 1
+        profile: 1,
+        status: 1
       }
-    }).fetch();
+    });
 
-    var playersConfig =  {
-      radius: (containerSize - avatarSize * 0.25) * 0.5 - avatarSize * 0.5,
-      centerX: containerSize * 0.5 - margin.left, // We need to adjust the translation of the drawing surface
-      centerY: containerSize * 0.5 - margin.top,
-      size: avatarSize,
-      count: bullseyePlayers.length
-    };
+    // We use a flag to indicate the initial query results that
+    // should not affect the 'added()' callback.
+    var initializing = true;
 
-    createPlayersCircle(bullseyePlayers, playersConfig);
+    templateInstance.playerHandle = bullseyePlayersCursor.observe({
+      added: function(doc) {
+        addToPlayers(doc);
+
+        // We want to prevent multiple calls of '()'
+        // while the 'added()' callback delivers the initial result of the query.
+        if (!initializing) {
+          createPlayersCircle(playerList, configPlayers(avatarSize, margin));
+        }
+      },
+      removed: function(doc) {
+        removeFromPlayers(doc);
+        createPlayersCircle(playerList, playersConfig);
+      },
+      changed: function(newDoc, oldDoc) {
+        // TODO change avatar
+      }
+    });
+
+    // At this point, 'observe' has returned and the initial query results are delivered.
+    // So we call '()' with the initial dataset.
+    initializing = false;
+    createPlayersCircle(playerList, playersConfig);
+
+    // var playersConfig =  {
+    //   radius: (containerSize - avatarSize * 0.25) * 0.5 - avatarSize * 0.5,
+    //   centerX: containerSize * 0.5 - margin.left, // We need to adjust the translation of the drawing surface
+    //   centerY: containerSize * 0.5 - margin.top,
+    //   size: avatarSize,
+    //   count: bullseyePlayers.length
+    // };
+
+    $(window).resize(function () {
+      Session.set("canvasSize", document.documentElement.clientHeight);
+      Session.set("playerRadius", (Session.get("canvasSize") - avatarSize * 0.25) * 0.5 - avatarSize * 0.5);
+      Session.set("centerX", Session.get("canvasSize") * 0.5);
+      Session.set("centerY", Session.get("canvasSize") * 0.5);
+      var config = configPlayers(avatarSize, margin);
+      d3.selectAll(".bullseye-player")
+      .attr("transform", function(d, i) {
+        // We translate each player to the center, then rotate each by
+        // its specific rotation angle, then translate by the radius along the y-axis.
+        return "translate(" + (config.centerX ) + "," + (config.centerY) + ")" +
+          "rotate(" + d.rotation + ")" +
+          "translate(0," + (-config.radius) + ")";
+      });
+    });
+
 
 
   }); // onRendered()
+
+
+  function configPlayers(avatar, margin) {
+    return {
+      radius: Session.get("playerRadius"),
+      centerX: Session.get("centerX") - margin.left,
+      centerY: Session.get("centerY") - margin.top,
+      size: avatar
+    };
+  }
+
+  function addToPlayers(doc) {
+    var newPlayer = doc;
+    switch(newPlayer.profile.name) {
+      // #1
+      case "Tester":
+        newPlayer.rotation = 30;
+        break;
+      // #2
+      case "Willy":
+        newPlayer.rotation = 90;
+        break;
+      // #3
+      case "Otto":
+        newPlayer.rotation = 150;
+        break;
+      // #4
+      case "Fred":
+        newPlayer.rotation = 210;
+        break;
+      // #5
+      case "Jenny":
+        newPlayer.rotation = 270;
+        break;
+      // #6
+      case "Kimmy":
+        newPlayer.rotation = 330;
+        break;
+      default:
+        // ignore
+        break;
+    }
+    playerList.push(newPlayer);
+  } // addToPlayers
+
+
+  function removeFromPlayers(doc) {
+    playerList.forEach(function(player, i, list) {
+      if (list[i] && list[i]._id === doc._id) {
+        playerList.splice(i, 1);
+      }
+    });
+  } // removeFromPlayers
+
 
   Template.bullseyePlayers.helpers({
     radius: function() {
@@ -60,17 +171,18 @@
 
   var createPlayersCircle = function(players, config) {
     var spacing = 15;
-    var theta = 2 * Math.PI / players.length;
+    // var theta = 2 * Math.PI / players.length;
 
-    var radialPlayers = [];
-    players.forEach(function(p, i, players) {
-      var radialPlayer, x, y, rotation;
-      x = config.centerX + config.radius * Math.cos(i * theta);
-      y = config.centerY + config.radius * Math.sin(i * theta);
-      rotation = 30 + i * 60;
-      radialPlayer = _.extend(p, {x:x}, {y:y}, {rotation: rotation});
-      radialPlayers.push(radialPlayer);
-    });
+    // var radialPlayers = [];
+    // players.forEach(function(p, i, players) {
+    // // players.forEach(function(p, i, players) {
+    //   var radialPlayer, x, y, rotation;
+    //   x = config.centerX + config.radius * Math.cos(i * theta);
+    //   y = config.centerY + config.radius * Math.sin(i * theta);
+    //   // rotation = 30 + i * 60;
+    //   radialPlayer = _.extend(p, {x:i}, {y:y}, {rotation: p.rotation});
+    //   radialPlayers.push(radialPlayer);
+    // });
 
     // =======================================
     // FOR DEBUGGING: big circle in the center
@@ -84,7 +196,8 @@
     //   .style("fill", "rgba(55, 55, 55, 0.3)");
 
     var playerElements = drawingSurface.selectAll(".bullseye-player")
-      .data(radialPlayers, function(d, i) {
+      // .data(radialPlayers, function(d, i) {
+      .data(players, function(d, i) {
         return d._id;
     });
 
@@ -133,5 +246,12 @@
         return d.profile.name;
       });
     };
+
+  // We need to stop observing our live queries when this template is
+  // removed from the DOM.
+  Template.bullseyePlayers.onDestroyed(function() {
+    var templateInstance = this;
+    templateInstance.playerHandle.stop();
+  }); // onDestroyed
 
 }()); // end function closure
