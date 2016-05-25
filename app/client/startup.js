9a9523ca3a718f1a5167e736bffc0bc36246b7b4
Nadja Zollo <nzollo@beuth-hochschule.de>
2016-05-25 20:21:54 +0200

    Disable webfont loading on startup.

--- app/client/startup.js
+++ app/client/startup.js
@@ -3,19 +3,19 @@
 // async loader for fonts
 // https://github.com/typekit/webfontloader
 
-Meteor.startup(function() {
-
-  WebFontConfig = {
-    google: { families: [ 'Short+Stack::latin' ] }
-  };
-  (function() {
-    var wf = document.createElement('script');
-    wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-    wf.type = 'text/javascript';
-    wf.async = 'true';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(wf, s);
-    // console.log("async fonts loaded", WebFontConfig);
-  })();
-
-});
+// Meteor.startup(function() {
+//
+//   WebFontConfig = {
+//     google: { families: [ 'Short+Stack::latin' ] }
+//   };
+//   (function() {
+//     var wf = document.createElement('script');
+//     wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+//     wf.type = 'text/javascript';
+//     wf.async = 'true';
+//     var s = document.getElementsByTagName('script')[0];
+//     s.parentNode.insertBefore(wf, s);
+//     // console.log("async fonts loaded", WebFontConfig);
+//   })();
+//
+// });
