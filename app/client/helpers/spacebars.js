8905c8bf4b93b27e6494e4c356d7123797a2f61c
Nadja Zollo <nadja.zollo@gmail.com>
2016-06-01 23:27:26 +0200

    Add global helpers to handle automode and trigger.

--- app/client/helpers/spacebars.js
+++ app/client/helpers/spacebars.js
@@ -10,3 +10,50 @@ Template.registerHelper("currentViewIs", function(currentView) {
 Template.registerHelper("viewportSize", function() {
   return Session.get("canvasSize") ? Session.get("canvasSize") : document.documentElement.clientHeight;
 });
+
+
+Template.registerHelper("isAutoMode", function() {
+  var bullseyeUser = Meteor.users.findOne({
+    roles: {
+      $in: ["view-bullseye"]
+    }
+  });
+  return bullseyeUser && bullseyeUser.profile.autoMode;
+});
+
+// Helper to switch away from 'splash' view if in 'automode'.
+Template.registerHelper("trigger", function() {
+  var bullseyeUser = Meteor.users.findOne({
+    roles: {
+      $in: ["view-bullseye"]
+    }
+  });
+
+  var reflectTrigger = bullseyeUser && bullseyeUser.profile.reflectTrigger;
+  var matchTrigger = bullseyeUser && bullseyeUser.profile.matchTrigger;
+
+  return Session.get("countMatches") >= matchTrigger || Session.get("countIds") >= reflectTrigger;
+});
+
+// Helper to detect which indicator to show if in 'automode'.
+Template.registerHelper("triggerIs", function(trigger) {
+  var bullseyeUser = Meteor.users.findOne({
+    roles: {
+      $in: ["view-bullseye"]
+    }
+  });
+
+  var reflectTrigger = bullseyeUser && bullseyeUser.profile.reflectTrigger;
+  var matchTrigger = bullseyeUser && bullseyeUser.profile.matchTrigger;
+
+  if (Session.get("countMatches") >= matchTrigger) {
+    return trigger === "match" && "match";
+  }
+
+  if (Session.get("countIds") >= reflectTrigger) {
+    return trigger === "reflect" && "reflect";
+  }
+
+  return;
+
+});
