5b61b1d69e136e2db19be9952123ce5439bb97a0
Nadja Zollo <nadja.zollo@gmail.com>
2016-06-02 22:29:43 +0200

    Add sidebar toggle.

--- admin/client/templates/includes/header/header.js
+++ admin/client/templates/includes/header/header.js
@@ -2,5 +2,9 @@ Template.header.events({
   "click #admin-logout": function(event, template) {
     event.preventDefault();
     AccountsTemplates.logout();
+  },
+  "click #sidebar-toggle": function(event, template) {
+    event.preventDefault();
+    $(".ui.sidebar").sidebar("toggle");
   }
 });
