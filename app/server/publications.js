76335bf88f61a2088a091e790856051efd8c0f1e
Nadja Zollo <nzollo@beuth-hochschule.de>
2016-08-18 12:09:53 +0200

    Publish debug messages collection.

--- app/server/publications.js
+++ app/server/publications.js
@@ -1,5 +1,9 @@
 (function() {
 
+Meteor.publish(null, function() {
+  return DebugMessages.find({});
+});
+
 // The 'Meteor.roles' collection is provided by the 'alanning:roles' package,
 // but it is not automatically published to the client.
 // Here, we make it available to every client and no subscription is required.
