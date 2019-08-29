916687a3242c6f52d0322d22fe00aaf79bf36589
Callum Macdonald <git@callum-macdonald.com>
2019-08-29 20:00:56 +0200

    Bugfix. Build admin to admin!

--- build_admin.sh
+++ build_admin.sh
@@ -6,6 +6,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 export PACKAGE_DIRS="${DIR}/global-packages/"
 
 cd admin
-meteor build --directory ../builds/app/ --architecture=os.linux.x86_64 --server localhost
+meteor build --directory ../builds/admin/ --architecture=os.linux.x86_64 --server localhost
 
 cp ../Dockerfile ../builds/admin/bundle
