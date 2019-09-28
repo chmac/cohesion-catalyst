0f914f085625c99725f91711d075e7f061fcb3a2
Callum Macdonald <git@callum-macdonald.com>
2019-09-28 17:57:51 +0200

    Remove debugger calls. #7

--- admin/client/templates/includes/export/export.js
+++ admin/client/templates/includes/export/export.js
@@ -1,8 +1,6 @@
 Template.export.events({
     "click #start_export"(event, tmpl) {
-        debugger;
         Meteor.call("getExportUrl", (error, response) => {
-            debugger;
             if (error) {
                 alert(`Error fetching export URL. ${error.message}`);
             } else {
