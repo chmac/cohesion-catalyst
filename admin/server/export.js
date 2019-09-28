fbccdbe4f0e1127a8865fd472c24a0595843a4d1
Callum Macdonald <git@callum-macdonald.com>
2019-09-28 17:32:32 +0200

    Save export requests to mongo. #7

--- admin/server/export.js
+++ admin/server/export.js
@@ -1,7 +1,14 @@
+const MAXIMUM_INTERVAL_FROM_REQUEST = 60 * 1e3; // 1 minute
+
+const Exports = new Mongo.Collection("exports");
+
 Meteor.methods({
     getExportUrl() {
-        const key = Mongo._CollectionPrototype._makeNewID();
-        return Meteor.absoluteUrl(`files/${key}`);
+        const exportId = Exports.insert({
+            userId: this.userId,
+            createdAt: Date.now()
+        });
+        return Meteor.absoluteUrl(`files/${exportId}`);
     }
 });
 
@@ -10,5 +17,15 @@ WebApp.connectHandlers.use("/files", (req, res, next) => {
         "content-type": "text/plain"
     });
     const key = req.url.substr(1);
+
+    const { userId, createdAt } = Exports.findOne(key) || {};
+    if (
+        !userId ||
+        !createdAt ||
+        Date.now() - createdAt > MAXIMUM_INTERVAL_FROM_REQUEST
+    ) {
+        throw new Error("Fail");
+    }
+
     res.end(`Booya we got you! Key was ${key}`);
 });
